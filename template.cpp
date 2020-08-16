41adedc93b37266d7920b896f13311f575562261
siddharth <smishra1605@gmail.com>
2020-08-16 08:33:53 +0530

    New Folder Structure

---
+++ template.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+#include <math.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <string>
+#include <bits/stdc++.h>
+#define endl "\n"
+#define ull unsigned long long
+#define MOD 1000000007
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+typedef long long int ll;
+ull PowMod(ull n)
+{
+	ull ret = 1;
+	ull a = 2;
+	while (n > 0) {
+		if (n & 1) ret = ret * a % MOD;
+		a = a * a % MOD;
+		n >>= 1;
+	}
+	return ret;
+}
+
+int main() {
+	FIO;
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ll tc;
+	cin >> tc;
+	while (tc--) {
+
+	}
+}
