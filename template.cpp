49d5abe575bbef9e33c8181ef23afc50bafc96b8
Hard-Coder05 <smishra1605@gmail.com>
2020-12-29 16:29:14 +0530

    Day 29

--- template.cpp
+++ template.cpp
@@ -5,19 +5,13 @@ Author: Siddharth Mishra
 GitHub: https://github.com/Hard-Coder05
 #############################################
 */
-#include <iostream>
-#include <cstdlib>
-#include <math.h>
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <utility>
-#include <iterator>
-#include <string>
 #include <bits/stdc++.h>
 #define endl "\n"
+#define max(a, b) (a < b ? b : a) 
+#define min(a, b) ((a > b) ? b : a) 
 #define ull unsigned long long
 #define MOD 1000000007
+#define INF 1000000000000000003
 #define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 using namespace std;
 typedef long long int ll;
