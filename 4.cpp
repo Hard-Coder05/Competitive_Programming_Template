51404158788d9970e9db1bcd8bd3dca4f5882d8c
Hard-Coder05 <smishra1605@gmail.com>
2021-01-04 12:08:51 +0530

    Day 35

---
+++ 4.cpp
@@ -0,0 +1,56 @@
+/*
+Problem Link: 
+#############################################
+Author: Siddharth Mishra
+GitHub: https://github.com/Hard-Coder05
+#############################################
+*/
+#include <bits/stdc++.h>
+#define endl "\n"
+#define max(a, b) (a < b ? b : a) 
+#define min(a, b) ((a > b) ? b : a) 
+#define ull unsigned long long
+#define MOD 1000000007
+#define INF 1000000000000000003
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+using namespace std;
+typedef long long int ll;
+int count(vector<ll> s)
+{
+    ll N, i, cnt = 0, ans = 0;
+    N = s.size();
+ 
+    for (i = 0; i < N; i++) {
+        if (s[i] <0)
+            cnt++;
+        if (s[i] >0)
+            ans += cnt;
+    }
+ 
+    return ans;
+}
+int main() {
+	FIO;
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ll tc;
+	cin >> tc;
+	while (tc--) {
+		ll n;
+		cin >> n;
+        vector<vector<ll>> v;
+        for(int i=0;i<n;i++){
+            ll m;
+            cin>>m;
+            for(int j=0;j<m;j++){
+                ll x;
+                cin>>x;
+                v[i].push_back(x);
+            }
+        }
+        if(n==1)
+        cout << count(v[0]) << endl;
+	}
+}
