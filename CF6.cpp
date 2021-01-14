e48cf6de78246a445620d34fa4544c1b0a4b3144
Hard-Coder05 <smishra1605@gmail.com>
2021-01-14 19:28:28 +0530

    Day idk

--- CF6.cpp
+++ CF6.cpp
@@ -1,30 +1,38 @@
-/*
-Problem Link: 
-#############################################
-Author: Siddharth Mishra
-GitHub: https://github.com/Hard-Coder05
-#############################################
-*/
 #include <bits/stdc++.h>
-#define endl "\n"
-#define max(a, b) (a < b ? b : a) 
-#define min(a, b) ((a > b) ? b : a) 
-#define MOD 1000000007
-#define INF 1000000000000000003
 #define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define endl "\n"
 using namespace std;
-typedef long long int ll;
+
+
 int main() {
-	FIO;
-#ifndef ONLINE_JUDGE
+    FIO;
+	#ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 #endif
-	ll tc;
-	cin >> tc;
-	while (tc--) {
-		ll n;
-		cin >> n;
-		cout<<n;
-	}
+    long long int n,m;
+    cin>>n>>m;
+    long long int a=INT_MIN;
+    long long int b=INT_MAX;
+    for(long long int i=0;i<m;i++){
+        long long int x,y;
+        cin>>x>>y;
+        if(x<y){
+            if(x>a)
+                a=x;
+            if(y<b)
+                b=y;
+        }
+        else{
+            if(y>a)
+                a=y;
+            if(x<b)
+                b=x;
+        }        
+    }    
+    if(m==0)
+        cout<<n-1<<endl;
+    else
+        cout<<b-a<<endl;
+    return 0;
 }
