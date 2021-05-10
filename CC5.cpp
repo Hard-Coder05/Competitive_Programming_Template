253e1c0ea4b7ea4b19e4f3cf78dd516f7c3eccdb
Hard-Coder05 <smishra1605@gmail.com>
2021-05-10 17:15:14 +0530

    added question

--- CC5.cpp
+++ CC5.cpp
@@ -26,9 +26,18 @@ using namespace std;
 	cout.tie(NULL);
 ll gcd(ll a, ll b)
 {
-	if (b == 0)
-		return a;
-	return gcd(b, a % b);
+	if (!a || !b)
+		return a | b;
+	unsigned shift = __builtin_ctz(a | b);
+	a >>= __builtin_ctz(a);
+	do
+	{
+		b >>= __builtin_ctz(b);
+		if (a > b)
+			swap(a, b);
+		b -= a;
+	} while (b);
+	return a << shift;
 }
 void solve(ll k)
 {
