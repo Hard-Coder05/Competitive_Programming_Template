9efe450cd8888d0f7b407a69f7de3d36bf2722fb
Hard-Coder05 <smishra1605@gmail.com>
2021-05-23 07:13:54 +0530

    cf722

--- CF2.cpp
+++ CF2.cpp
@@ -36,5 +36,20 @@ int main()
 	cin >> tc;
 	while (tc--)
 	{
+		ll n;
+		cin >> n;
+		vector<ll> arr;
+		for (ll i = 0; i < n; i++)
+			cin >> arr[i];
+		sort(arr, arr + n);
+		ll diff[n - 1];
+		diff[0] = arr[1] - arr[0];
+		for (ll i = 1; i < n - 1; i++)
+			diff[i] = min(diff[i - 1], arr[i + 1] - arr[i]);
+		ll i = 0;
+		for (ll i = 0; i < n; i++)
+			if (arr[i] > diff[i])
+				break;
+		cout << i << endl;
 	}
 }
